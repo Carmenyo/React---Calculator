f6f79b70542c033375e81bf4cde07ea7031b982c
Carmenyo <carmencastroyo@gmail.com>
2022-04-30 17:21:00 +0200

    test automatic saving

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+
+import '../styles/App.scss';
+import Boton from './Boton';
+import Pantalla from "./Pantalla"
+import BotonClear from './BotonClear';
+import {useState} from "react";
+import {evaluate} from "mathjs";
+
+function App() {
+
+   const [input, setInput] = useState ("");
+
+   const agregarInput = valor => {
+  setInput(input + valor);
+   };
+
+   const calcularResultado = () =>{
+     setInput(evaluate(input));
+   };
+
+  return (
+  
+    <div className='App'>
+    <div className='contenedor-calculadora'>
+    <Pantalla input={input}/>
+    <div className='fila'>
+      <Boton manejarClic= {agregarInput}>1</Boton>
+      <Boton manejarClic= {agregarInput}>2</Boton>
+      <Boton manejarClic= {agregarInput}>3</Boton>
+      <Boton manejarClic= {agregarInput}>+</Boton>
+    </div>
+    <div className='fila'>
+      <Boton manejarClic= {agregarInput}>4</Boton>
+      <Boton manejarClic= {agregarInput}>5</Boton>
+      <Boton manejarClic= {agregarInput}>6</Boton>
+      <Boton manejarClic= {agregarInput}>-</Boton>
+    </div>
+    <div className='fila'>
+      <Boton manejarClic= {agregarInput}>7</Boton>
+      <Boton manejarClic= {agregarInput}>8</Boton>
+      <Boton manejarClic= {agregarInput}>9</Boton>
+      <Boton manejarClic= {agregarInput}>*</Boton>
+    </div>
+    <div className='fila'>
+      <Boton manejarClic= {calcularResultado}>=</Boton>
+      <Boton manejarClic= {agregarInput}>0</Boton>
+      <Boton manejarClic= {agregarInput}>.</Boton>
+      <Boton manejarClic= {agregarInput}>/</Boton>
+    </div>
+    <div className='fila'></div>
+    <BotonClear manejarClear= {() => setInput ("")}>
+      Clear
+    </BotonClear>
+    </div>
+    </div>
+  
+  
+    
+  );
+}
+
+export default App;
---
+++ src/components/Pantalla.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Pantalla.scss"
+
+const Pantalla = ({ input}) => (
+    
+    <div className="input">
+        {input}
+    </div>
+);
+
+export default Pantalla;
